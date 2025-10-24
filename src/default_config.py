fdaa06dcf04f7d9686635ba833d7ee425049c74f
Melissa Belvadi <mbelvadi@upei.ca>
2025-10-24 10:46:12 -0300

    removed data_table

--- src/default_config.py
+++ src/default_config.py
@@ -1,7 +1,6 @@
 #####  Various constant values that remain constant
 
 sqlite_filename = 'counterdata.db'
-data_table = 'usage_data'
 error_log_file = 'infolog.txt'
 json_dir = 'json_folders'
 tsv_dir = 'tsv_folders'
