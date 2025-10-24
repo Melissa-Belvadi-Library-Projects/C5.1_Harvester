2f14078922580fb67e478b89ad25507f24f13d7c
Melissa Belvadi <mbelvadi@upei.ca>
2025-10-24 10:44:06 -0300

    took out data table

--- src/current_config.py
+++ src/current_config.py
@@ -1,11 +1,10 @@
 #####  Various constant values that you can change as you like
 
 sqlite_filename = 'counterdata.db'
-data_table = 'usage_data'
 error_log_file = 'infolog.txt'
 json_dir = 'json_folders'
 tsv_dir = 'tsv_folders'
-providers_file = 'providers_all.tsv'
-save_empty_report = False
+providers_file = 'providers.tsv'
+save_empty_report = True
 always_include_header_metric_types = True
 default_begin = '2025-01'
